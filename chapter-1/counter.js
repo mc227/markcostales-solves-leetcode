2ba5d7a1d532b83f9e4531fe64fe05ad69310696
mc227 <markcostales.mark@gmail.com>
2025-10-15 14:07:03 -0400

    i completed the 1st chapter of JavaScript Data Structures and Algorithms: An Introduction to Understanding and Implementing Core Data Structure and Algorithm Fundamentals
    Sammie Bae

--- chapter-1/counter.js
+++ chapter-1/counter.js
@@ -1,9 +1,88 @@
-function counter(n){
-    let count = 0
-    for(let i = 0; i < n; i++){
-        count += 1
-    }
-    return count
-}
+// function counter(n){
+//     let count = 0
+//     for(let i = 0; i < n; i++){
+//         count += 1
+//     }
+//     return count
+// }
 
-console.log(counter(5))
+// console.log(counter(5))
+
+
+// function counter(n){
+//     let count = 0
+//     for(let i = 0; i < 5*n; i++){
+//         count += 1
+//     }
+//     return count
+// }
+
+// console.log(counter(5))
+
+// function counter(n){
+//     let count = 0
+//     for(let i = 0; i < n; i++){
+//         count += 3
+//     }
+//     return count
+// }
+
+// console.log(counter(5))
+
+/**
+ * example of sum rule, adding up big-Os
+ */
+
+// function a(n){
+//     let count = 0
+
+//     for(let i = 0; i < n; i++){
+//         count += 1
+//     }
+
+//     for(let i = 0; i < 5*n; i++){
+//         count += 1
+//     }
+
+//     return count
+// }
+
+// console.log(a(5))
+
+/**my attempt to create a nested loop to show O of n squared */
+
+// function a(n){
+//     let count = 0
+
+//     for(let i = 0; i < n; i++){
+//         for(let i = 0; i < 5*n; i++){
+//             count += 1
+//         }
+//     }
+
+//     return count
+// }
+
+// console.log(a(5))
+
+// function a(n){
+//     let count = 0
+
+//     for(let i = 0; i < n*n; i++){
+//         count += 1
+//     }
+    
+
+//     return count
+// }
+
+// console.log(a(5))
+
+/**
+ 1) O of n squared
+ 2) O of n cubed
+ 3) O of 1
+ 4) O of n
+ 5) O log of n
+ 6) O of inifinity
+ */
