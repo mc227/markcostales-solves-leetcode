b4753e265a213c267a44b231052e4485601f14e5
mc227 <markcostales.mark@gmail.com>
2025-10-17 13:18:54 -0400

    since the shutdown will be longer, i'm gonna extend my studies to python dsa

---
+++ python/README.md
@@ -0,0 +1,10 @@
+
+To activate virtual environment:
+```bash
+source dsa/bin/activate
+```
+
+To deactivate when you're done:
+```bash
+deactivate
+```
---
+++ python/binary_search.py
@@ -0,0 +1,8 @@
+def binary_search(arr, target):
+    l,h = 0, len(arr) - 1
+    
+    return -1
+
+if __name__ == "__main__":
+    myArray = [1,2,3,4,5]
+    print(binary_search(myArray,3))
