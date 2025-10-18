56009616676768fe876739b975d19320aa86da11
Mark <markcostales.mark@gmail.com>
2025-10-18 09:41:00 -0400

    i struggled through it a bit. i need to review it tomorrow: g

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,8 +1,20 @@
 def binary_search(arr, target):
     l,h = 0, len(arr) - 1
-    
+     
+    while l <= h:
+        mid = int(h+l)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            l = mid + 1
+        else:
+            h = mid - 1
     return -1
 
 if __name__ == "__main__":
     myArray = [1,2,3,4,5]
+    print(binary_search(myArray,1))
+    print(binary_search(myArray,2))
     print(binary_search(myArray,3))
+    print(binary_search(myArray,4))
+    print(binary_search(myArray,5))
