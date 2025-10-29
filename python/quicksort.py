17b882aab88b42fa9840b9ad505083d43d375cea
mc227 <markcostales.mark@gmail.com>
2025-10-29 10:36:02 -0400

    quicksort 2 time 2 time implementation

--- python/quicksort.py
+++ python/quicksort.py
@@ -1,18 +1,16 @@
 def quicksort(arr):
-    # case where array is blank or single
-    if len(arr) <= 1:
+    # cover the case where array is null or just on
+    if len(arr) <=1:
         return arr
-    # calculate the middle and set it to pivot
-    pivot = arr[len(arr)//2]
-    # calculate the numbers below the middle
-    left = [x for x in arr if x < pivot]
-    # calcualte the middle number
-    middle = [x for x in arr if x == pivot]
-    # calculate the numbers on the right
-    right = [x for x in arr if x > pivot]
-    # add them all up
-    return quicksort(left) + middle + quicksort(right)
 
+    # define a pivot, which is the middle of the array
+    pivot = arr[len(arr)//2]
+    # divide the array into left,middle and right
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    # do some recursion and stuff
+    return quicksort(left) + middle + quicksort(right)
 if __name__ == "__main__":
-    numbers = [5,4,3,2,1]
-    print(quicksort(numbers))
+    print(quicksort([5,4,3,2,1]))
