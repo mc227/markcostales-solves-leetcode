837b93b2dff9a5e8777fda2208e833e08d10ac9f
mc227 <markcostales.mark@gmail.com>
2025-10-29 10:11:37 -0400

    was able to create binary search and quicksort from scratch. doing it again today

--- python/quicksort.py
+++ python/quicksort.py
@@ -1,28 +1,18 @@
-# i want to do a quick implentation of the the quicksort algorithm
-
-# there are some recursion going on
-# we cover the case when the the array is one or none
-
-# then we look for the middle of the array if it exists
-
-# this middle value we return the middle value
-# i personally don't know what would happen if the case were odd or even
-# then we define pivot
-# then we define left
-# then we define right
-# then we return the quicksort left + middle + quicksort right
-
 def quicksort(arr):
-
+    # case where array is blank or single
     if len(arr) <= 1:
         return arr
-
+    # calculate the middle and set it to pivot
     pivot = arr[len(arr)//2]
-    left = [item for item in arr if item < pivot ]
-    middle = [item for item in arr if item == pivot]
-    right = [item for item in arr if item > pivot]
+    # calculate the numbers below the middle
+    left = [x for x in arr if x < pivot]
+    # calcualte the middle number
+    middle = [x for x in arr if x == pivot]
+    # calculate the numbers on the right
+    right = [x for x in arr if x > pivot]
+    # add them all up
     return quicksort(left) + middle + quicksort(right)
 
 if __name__ == "__main__":
-    numbers = [6,5,4,3,2,1]
+    numbers = [5,4,3,2,1]
     print(quicksort(numbers))
