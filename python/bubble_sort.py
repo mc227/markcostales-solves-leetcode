639fd600d904026e418078433854825e417faae5
Mark <markcostales.mark@gmail.com>
2025-11-01 22:46:13 -0400

    i was working on chapter 1 exercise 3 and it called for the bubble sort. I've done bubble sort before but i've forgetten it

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,5 +1,15 @@
-
+def bubble_sort(arr):
+    # basically i need two for loops
+    # get the first value and compare it to elements 2 to the last
+    # next loop it's second element compared to element 3 to the last
+    # next its third element compared to element 4 to the last
+    # i need to find a way to go through the list like that
+    for i in range(len(arr)):
+        print(f"i {i}")
+    return arr
 
 
 if __name__ == "__main__":
-    print("hello world")
+    numbers = [4,1,5,3,2]
+    # 1 2 3 4 5
+    print(bubble_sort(numbers))
