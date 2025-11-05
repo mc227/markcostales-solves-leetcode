df6d3b21bec50dd7754026e413b34f9da56996af
mc227 <markcostales.mark@gmail.com>
2025-11-05 12:13:00 -0500

    unoptimized bubble sort without early exit optimization

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,15 +1,16 @@
 def bubble_sort(arr):
-    # basically i need two for loops
-    # get the first value and compare it to elements 2 to the last
-    # next loop it's second element compared to element 3 to the last
-    # next its third element compared to element 4 to the last
-    # i need to find a way to go through the list like that
-    for i in range(len(arr)):
-        print(f"i {i}")
+
+    for j in range(len(arr)-1):
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+
+
     return arr
 
 
 if __name__ == "__main__":
-    numbers = [4,1,5,3,2]
-    # 1 2 3 4 5
+    numbers = [3,5,1,4,2]
     print(bubble_sort(numbers))
