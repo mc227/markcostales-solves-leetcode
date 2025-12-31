ca21a51d62df75cef4670d7b787f867e20c4433f
mc227 <markcostales.mark@gmail.com>
2025-12-31 15:05:56 -0500

    feat: implement bubble sort with early termination optimization

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,14 +1,23 @@
 def bubble_sort(arr):
+    # loop through each array
+    # so for loop length is length of the array
+    # do so double for loop
+    # in the inner for loop i compare the item to the one beside it
+    # if it is bigger, that is  if j > j+1, then swap them
+    # the case above is the worst case scenario and stuff
+    # i have to add a state whether the item has been swapped
+    # basically it is a way to find out if i need to go on further and stuff
     n = len(arr)
 
     for i in range(n):
         swapped = False
-        for j in range(n-1-i):
+        for j in range(n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 swapped = True
-        if not swapped:
+        if swapped == False:
             break
     return arr
 if __name__ == "__main__":
-    print(bubble_sort([1,4,3,2,5]))
+    arr = [5,4,3,2,1]
+    print(bubble_sort(arr))
