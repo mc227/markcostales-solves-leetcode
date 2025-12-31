22595e825af01deda21da6edd5d299f38286e0f0
mc227 <markcostales.mark@gmail.com>
2025-12-31 14:49:48 -0500

    it's been a long time

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -3,20 +3,12 @@ def bubble_sort(arr):
 
     for i in range(n):
         swapped = False
-
-        for j in range(n-i-1):
+        for j in range(n-1-i):
             if arr[j] > arr[j+1]:
-                temp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1] = temp
+                arr[j], arr[j+1] = arr[j+1], arr[j]
                 swapped = True
         if not swapped:
             break
-
     return arr
-
-
-
 if __name__ == "__main__":
-    numbers = [4,2,8,1,5]
-    print(bubble_sort(numbers))
+    print(bubble_sort([1,4,3,2,5]))
---
+++ python/selection_sort.py
@@ -0,0 +1,13 @@
+def selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1,n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+if __name__ == "__main__":
+    print(selection_sort([5,4,3,2,1]))
+
