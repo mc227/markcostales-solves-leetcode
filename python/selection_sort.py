d00a0cd4b5998c4e27ae27128ff0d8f50287ad72
mc227 <markcostales.mark@gmail.com>
2026-01-01 15:46:55 -0500

    working on selection sort

--- python/selection_sort.py
+++ python/selection_sort.py
@@ -1,13 +1,7 @@
 def selection_sort(arr):
-    n = len(arr)
-
-    for i in range(n):
-        min_idx = i
-        for j in range(i+1,n):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
     return arr
-if __name__ == "__main__":
-    print(selection_sort([5,4,3,2,1]))
 
+
+if __name__ == "__main__":
+    for i in range(1,5):
+        print(i)
