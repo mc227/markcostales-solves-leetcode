ccda92e83fff24a02341093369527f4c8ca1c8bb
mc227 <markcostales.mark@gmail.com>
2026-01-06 16:19:12 -0500

    insertion and selection sort

--- python/insertion_sort.py
+++ python/insertion_sort.py
@@ -1,14 +1,17 @@
 def insertion_sort(arr):
-    for i in range(1,len(arr)):
+
+    for i in range(1, len(arr)):
         key = arr[i]
+
         j = i - 1
 
-        while j >= 0 and key < arr[j]:
+        while j >= 0 & key < arr[j]:
             arr[j+1] = arr[j]
             j -= 1
 
         arr[j+1] = key
+
     return arr
 
 if __name__ == "__main__":
-    print(insertion_sort([5,3,4,2,1]))
+    print(insertion_sort([5,4,3,2,1]))
