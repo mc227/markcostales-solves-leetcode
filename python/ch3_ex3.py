ab57cb9bd9b6ab7738e65ea06f3c415d03aa6ee3
mc227 <markcostales.mark@gmail.com>
2026-03-19 11:48:10 -0400

    zero out

--- python/ch3_ex3.py
+++
@@ -1,7 +0,0 @@
-if __name__ == "__main__":
-    elements = {
-            1:"Carbon",
-            2:"Helium"
-            }
-
-    print(elements[2])
--- python/countdown_gen.py
+++
@@ -1,9 +0,0 @@
-def countdown_gen(num):
-    while num > 0:
-        yield num
-        num -= 1
-
-if __name__ == "__main__":
-    
-    for i in countdown_gen(3):
-        print(i)
