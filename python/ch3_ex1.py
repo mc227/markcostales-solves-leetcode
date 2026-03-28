954e79095587dd40691933b8603c631474dc82ee
Mark <markcostales.mark@gmail.com>
2026-03-28 13:37:07 -0400

    zero out

--- python/ch3_ex1.py
+++
@@ -1,4 +0,0 @@
-if __name__ == "__main__":
-    my_list = [3,5,7,11,13,17,19,23,29,31]
-    subset = my_list[2:7]
-    print(subset)
--- python/ch3_ex2.py
+++
@@ -1,3 +0,0 @@
-if __name__ == "__main__":
-    my_vowels = {'a','b','c','d','e'}
-    print("a" in my_vowels)
